6ad61ec8971b1acafa00a3a6021791af41f348c7
kevin lang <langkevina@gmail.com>
2019-06-15 21:09:35 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,7 +6,16 @@ class Triangle
     @side2 = side2
     @side3 = side3
   end
+  def aboveZero
+    if side1 > 0 && side2 >0 && side3 > 0
+      return true
+    end
+    false
+  end
   def isEquilateral
+    if !aboveZero
+      return false
+    end
     if (side1 == side2) && (side2 == side3)
       true
     else
@@ -14,6 +23,12 @@ class Triangle
     end
   end
   def isIsosceles
+    if !aboveZero
+      return false
+    end
+    if side1 < 3
+      return false
+    end
     if (side1 == side2) || (side2 == side3) || (side1 == side3)
       return true
     else
@@ -21,8 +36,19 @@ class Triangle
     end
   end
   def isScalene
-    if ()(side1 != side2) && (side2 != side3) && (side1 != side3)) && ((side2 - side1 == -1) || (side2 - side1 == -1))
-      return true
+    # if ((side1 != side2) && (side2 != side3) && (side1 != side3)) && ((side2 - side1 == -1) || (side2 - side1 == -1))
+    min1 = -0.10000000000000003
+    if !aboveZero
+      return false
+    end
+
+    if ((side1 != side2) && (side2 != side3) && (side1 != side3))
+      if (side2 - side1 == -1) || (side1 - side2 == -1) || (side3 - side1 == -1)  || (side1 - side3 == -1)  ||
+         (side2 - side1 == min1) || (side1 - side2 == min1) || (side3 - side1 == min1) || (side1 - side2 == min1)
+        return true
+      else
+        return false
+      end
     else
       return false
     end
@@ -34,6 +60,16 @@ class Triangle
       return "isosceles"
     elsif isScalene
       return "scalene"
+    else
+        begin
+            raise TriangleError
+            rescue TriangleError => error
+                puts error.message
+        end
+
     end
+  end
+  class TriangleError < StandardError
+
   end
 end
